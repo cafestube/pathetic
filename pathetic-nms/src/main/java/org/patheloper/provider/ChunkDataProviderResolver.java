561dfae1794659445344e93a0afa1598aa83bc76
David Mayr <davidliebtkekse@gmail.com>
2024-08-24 14:24:17 +0200

    feat: rip out nms logic since we only use paper, and it makes compiling easier when we push to our own repo

--- pathetic-nms/src/main/java/org/patheloper/provider/ChunkDataProviderResolver.java
+++ pathetic-nms/src/main/java/org/patheloper/provider/ChunkDataProviderResolver.java
@@ -3,21 +3,7 @@ package org.patheloper.provider;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.patheloper.api.snapshot.ChunkDataProvider;
-import org.patheloper.provider.v1_12.v1_12ChunkDataProviderImpl;
-import org.patheloper.provider.v1_15.v1_15ChunkDataProviderImpl;
-import org.patheloper.provider.v1_16.v1_16ChunkDataProviderImpl;
-import org.patheloper.provider.v1_17.v1_17ChunkDataProviderImpl;
-import org.patheloper.provider.v1_18.v1_18ChunkDataProviderImpl;
-import org.patheloper.provider.v1_18_R2.v1_18_R2ChunkDataProviderImpl;
-import org.patheloper.provider.v1_19_R2.v1_19_R2_ChunkDataProviderImpl;
-import org.patheloper.provider.v1_19_R3.v1_19_R3ChunkDataProviderImpl;
-import org.patheloper.provider.v1_20_R1.v1_20_R1ChunkDataProviderImpl;
-import org.patheloper.provider.v1_20_R2.v1_20_R2ChunkDataProviderImpl;
-import org.patheloper.provider.v1_20_R3.v1_20_R3ChunkDataProviderImpl;
-import org.patheloper.provider.v1_20_R4.v1_20_R4ChunkDataProviderImpl;
-import org.patheloper.provider.v1_8.v1_8ChunkDataProviderImpl;
 import org.patheloper.provider.paper.PaperChunkDataProvider;
-import org.patheloper.v1_21ChunkDataProviderImpl;
 
 @Getter
 @Slf4j
@@ -31,68 +17,10 @@ public class ChunkDataProviderResolver {
     if (isPaper()) {
       chunkDataProvider = new PaperChunkDataProvider();
     } else {
-      chunkDataProvider = determineChunkDataProvider(major, minor);
+      throw new IllegalStateException("Unsupported server version: " + version);
     }
 
-    log.info(
-        "Detected version v{}, using {}", version, chunkDataProvider.getClass().getSimpleName());
-  }
-
-  private ChunkDataProvider determineChunkDataProvider(int major, int minor) {
-    final ChunkDataProvider chunkDataProvider;
-    switch (major) {
-      case 21:
-        chunkDataProvider = new v1_21ChunkDataProviderImpl();
-        break;
-      case 20:
-        if (minor == 5 || minor == 6) {
-          chunkDataProvider = new v1_20_R4ChunkDataProviderImpl();
-          break;
-        } else if (minor == 3 || minor == 4) {
-          chunkDataProvider = new v1_20_R3ChunkDataProviderImpl();
-          break;
-        } else if (minor == 2) {
-          chunkDataProvider = new v1_20_R2ChunkDataProviderImpl();
-          break;
-        } else if (minor == 1) {
-          chunkDataProvider = new v1_20_R1ChunkDataProviderImpl();
-          break;
-        }
-      case 19:
-        if (minor == 2 || minor == 3) {
-          chunkDataProvider = new v1_19_R2_ChunkDataProviderImpl();
-          break;
-        }
-        if (minor == 4) {
-          chunkDataProvider = new v1_19_R3ChunkDataProviderImpl();
-          break;
-        }
-      case 18:
-        if (minor == 2) {
-          chunkDataProvider = new v1_18_R2ChunkDataProviderImpl();
-          break;
-        }
-        chunkDataProvider = new v1_18ChunkDataProviderImpl();
-        break;
-      case 17:
-        chunkDataProvider = new v1_17ChunkDataProviderImpl();
-        break;
-      case 16:
-        chunkDataProvider = new v1_16ChunkDataProviderImpl();
-        break;
-      case 15:
-        chunkDataProvider = new v1_15ChunkDataProviderImpl();
-        break;
-      case 12:
-        chunkDataProvider = new v1_12ChunkDataProviderImpl();
-        break;
-      case 8:
-        chunkDataProvider = new v1_8ChunkDataProviderImpl();
-        break;
-      default:
-        throw new IllegalArgumentException("Unsupported version: " + major + "." + minor);
-    }
-    return chunkDataProvider;
+    log.info("Detected version v{}, using {}", version, chunkDataProvider.getClass().getSimpleName());
   }
 
   private boolean isPaper() {
